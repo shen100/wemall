0d30cf0295bde112ebd15c3eaa55fb1f9322c99c
wgz <675611281@qq.com>
2017-02-08 14:12:33 +0800

    上传首页

--- client/javascripts/admin/overview/components/Index.vue
+++ client/javascripts/admin/overview/components/Index.vue
@@ -1,32 +1,97 @@
 <template>
     <div id="app">
         <admin-header/>
-        <div>
+        <div class="wemall-admin-body">
             <admin-sidebar/>
             <div class="wemall-admin-content">
-                
+                <div class="wemall-page-card-container">
+                    <admin-card
+                        v-for="item in cardItems"
+                        :cardTitle="item.cardTitle"
+                        :cardNum="item.cardNum"
+                        :imageUrl="item.imageUrl"
+                    />
+                </div>
+                <div class="wemall-admin-info">
+                    <p class="wemall-admin-info-title">系统公告</p>
+                    <p class="wemall-admin-info-content">暂无消息</p>
+                </div>
+                <div class="wemall-admin-info">
+                    <p class="wemall-admin-info-title">系统信息</p>
+                    <p class="wemall-admin-info-content">
+                        <span class="wemall-admin-info-item">程序版本：</span>
+                        <span>{{wemall.name}}</span>
+                    </p>
+                    <p class="wemall-admin-info-content">
+                        <span class="wemall-admin-info-item">当前版本：</span>
+                        <span>{{wemall.version}}</span>
+                    </p>
+                    <p class="wemall-admin-info-content">
+                        <span class="wemall-admin-info-item">最新版本：</span>
+                        <span>{{wemall.latestVer}}</span>
+                    </p>
+                    <p class="wemall-admin-info-content">
+                        <span class="wemall-admin-info-item">官方网址：</span>
+                        <span>{{wemall.officialUrl}}</span>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style>
-   
+    .wemall-admin-content {
+        padding: 20px;
+    }
+    .wemall-page-card-container {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
 </style>
 
 <script>
     import Header     from '../../components/Header.vue';
     import Sidebar    from '../../components/Sidebar.vue';
+    import ThemeCard  from './ThemeCard.vue';
 
     export default {
         data() {
+            let wemall = jsonData.wemall || {};
             return {
-                
+                cardItems: [
+                    {
+                        cardTitle: '新订单',
+                        cardNum  : jsonData.todayOrder,
+                        imageUrl : '/images/cart.png'
+                    }, {
+                        cardTitle: '新销售额',
+                        cardNum  : jsonData.todaySale,
+                        imageUrl : '/images/dollar.png'
+                    }, {
+                        cardTitle: '总用户',
+                        cardNum  : jsonData.totalOrder,
+                        imageUrl : '/images/account-filling.png'
+                    }, {
+                        cardTitle: '总订单',
+                        cardNum  : jsonData.totalSale,
+                        imageUrl : '/images/trade.png'
+                    }
+                ],
+                wemall: {
+                    latestVer: wemall.latestVer || '',
+                    name: wemall.name || '',
+                    officialUrl: wemall.officialUrl || '',
+                    version: wemall.version || ''
+                }
             };
         },
         components: {
             'admin-header'  : Header,
-            'admin-sidebar' : Sidebar
+            'admin-sidebar' : Sidebar,
+            'admin-card'    : ThemeCard
         }
     }
 </script>
---
+++ client/javascripts/admin/overview/components/ThemeCard.vue
@@ -0,0 +1,109 @@
+<template>
+	<div class="theme-card-container">
+		<div class="theme-card-inner">
+			<div class="theme-card-inner-num">{{cardNum}}</div>
+			<div class="theme-card-inner-title">{{cardTitle}}</div>
+		</div>
+		<img :src="imageUrl" class="theme-card-icon"></img>
+		<a :href="linkUrl" class="theme-card-link">
+			<span class="theme-card-link-label">更多详情<i class="el-icon-caret-right"></i></span>
+		</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ThemeCard',
+		props: {
+			cardNum: {
+				type: Number,
+				default: 0
+			},
+			cardTitle: {
+				type: String,
+				default: ''
+			},
+			imageUrl: {
+				type: String,
+				default: ''
+			},
+			linkUrl: {
+				type: String,
+				default: '#'
+			}
+		}
+	}
+</script>
+
+<style>
+	.theme-card-container {
+		height: 130px;
+    	width: 24%;
+    	min-width: 220px;
+    	margin-right: 10px;
+		position: relative;
+		background-color: #e7edf7;
+		border-radius: 10px;
+		color: #1296db;
+		-webkit-transition: all .3s linear;
+		-o-transition: all .3s linear;
+		transition: all .3s linear;
+	}
+	.theme-card-container:hover .theme-card-link {
+		height: 35px;
+		line-height: 35px;
+	}
+	.theme-card-container:hover .theme-card-icon {
+		height: 70px;
+		width: 70px;
+		top: 10px;
+	}
+	.theme-card-container:hover .theme-card-inner {
+		margin-left: 10px;
+	}
+	.theme-card-container .theme-card-icon {
+		width: 60px; 
+		height: 60px;
+		position: absolute;
+		right: 10px;
+    	top: 20px;
+    	-webkit-transition: all .3s linear;
+		-o-transition: all .3s linear;
+		transition: all .3s linear;
+	}
+	.theme-card-inner {
+		margin-top: 10px;
+		padding: 10px;
+		-webkit-transition: all .3s linear;
+		-o-transition: all .3s linear;
+		transition: all .3s linear;
+	}
+	.theme-card-inner-num {
+		font-weight: bold;
+		font-size: 30px;
+	}
+	.theme-card-inner-title {
+		font-weight: bold;
+		font-size: 18px;
+	}
+	.theme-card-link {
+		background-color: rgba(33,33,33,0.5);
+		height: 30px;
+		text-align: center;
+		display: block;
+		text-decoration: none;
+		line-height: 30px;
+		border-bottom-right-radius: 10px;
+	    border-bottom-left-radius: 10px;
+	    position: absolute;
+	    bottom: 0;
+	    width: 100%;
+	    -webkit-transition: all .3s linear;
+		-o-transition: all .3s linear;
+		transition: all .3s linear;
+	}
+	.theme-card-link-label {
+		font-size: 14px;
+		color: #fff;
+	}	
+</style>
