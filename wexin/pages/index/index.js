60c8909ce92053e77e542ac4c12fd73385f3e78b
shen100 <liushen_shen@163.com>
2017-05-21 15:38:15 +0800

    微信小程序首页页面完成

--- wexin/pages/index/index.js
+++ wexin/pages/index/index.js
@@ -1,26 +1,94 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    data: {
+        categories: [
+            {
+                "id": 1,
+                "name": '奶粉'
+            },
+            {
+                "id": 2,
+                "name": '尿不湿'
+            },
+            {
+                "id": 3,
+                "name": '澳大利亚swisse'
+            },
+            {
+                "id": 4,
+                "name": '澳大'
+            },
+            {
+                "id": 5,
+                "name": '澳大利亚swisse'
+            },
+            {
+                "id": 6,
+                "name": '澳大利亚swisse'
+            },
+            {
+                "id": 7,
+                "name": '澳大利亚swisse'
+            }
+        ],
+        categoryIndex: 0,
+        products: [
+            {
+                "id": 1,
+                "name": "testin超级AB测试",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 2,
+                "name": "testin超级AB测试",
+                "url": "https:\/\/img.yzcdn.cn\/upload_files\/2017\/03\/21\/FhYyxdpE9iQU2V1HIwqUf4H8jOXs.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 3,
+                "name": "测试一下工工fadi地3",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 4,
+                "name": "测试一下工工fadi地4",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 5,
+                "name": "测试一下工工fadi地5",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 6,
+                "name": "测试一下工工fadi地6",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 7,
+                "name": "测试一下工工fadi地7",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 8,
+                "name": "测试一下工工fadi地8",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            },
+            {
+                "id": 9,
+                "name": "测试一下工工fadi地9",
+                "url": "../../images/1.jpg",
+                "price": 23.44
+            }
+        ]
+    },
+    onLoad: function() {
+      
+    }
 })
